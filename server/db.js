df67996e7d1dd288cedf07860282b96b6063f47f
Layandabbah-telhai <layandab@m.telhai.ac.il>
2026-03-26 16:08:09 +0200

    fix db connection

--- server/db.js
+++ server/db.js
@@ -1,6 +1,13 @@
 const mysql = require("mysql2/promise");
 
 function createPoolFromEnv() {
+  console.log("DB ENV:", {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+  });
+
   return mysql.createPool({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT || 3306),
@@ -9,9 +16,8 @@ function createPoolFromEnv() {
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
     ssl: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // 🚨 REQUIRED for Railway
     },
   });
 }
