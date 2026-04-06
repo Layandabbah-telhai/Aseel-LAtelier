0f50582a5fc2d55a9e6bbc49cf5eccea7a3f3650
Layandabbah-telhai <layandab@m.telhai.ac.il>
2026-04-06 23:16:57 +0300

    Add .gitignore, update database configuration in .env, and clean up SQL dump

--- aseel_atelier_db.sql
+++ aseel_atelier_db.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `wedding_dresses_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE `wedding_dresses_db`;
+
 -- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
 --
 -- Host: localhost    Database: wedding_dresses_db
